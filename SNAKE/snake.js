d1fc189a35b5d3b2a52244652ec3fce7fb43e33d
Jarek Pieprzak <jarekpiep@gmail.com>
2017-12-12 00:29:17 +0100

    create variables

---
+++ SNAKE/snake.js
@@ -0,0 +1,25 @@
+// Planowanie zmiennych
+
+    // ustawienia
+    var snakeX = 2;
+    var snakeY = 2;
+    var height = 30;
+    var width = 30;
+    var interval = 100;
+    var increment = 1;
+
+
+
+    // zmienne gry
+    var tailX = [snakeX];
+    var tailY = [snakeY];
+    var fX;
+    var fY;
+    var running;
+    var gameOver;
+    var direction = -1; // up = 0, down = -1, left = 1, right = 2
+    var interval = 100;
+    var int;
+
+
+
